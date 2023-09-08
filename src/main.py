38ebf5bc968b591ec29fae4aef89a650b9980223
Rin <31349569+NagaseIori@users.noreply.github.com>
2023-09-08 09:06:15 +0800

    add auto bonus

--- src/main.py
+++ src/main.py
@@ -3,6 +3,12 @@ import cv2
 import time
 import subprocess
 import random
+import pytesseract
+import re
+from PIL import Image
+
+__last_pos = False
+_temp_path = "temp/temp.png"
 
 
 def adb_run(serial_num, command):
@@ -16,11 +22,13 @@ def adb_run(command):
 
 
 def adb_screenshot():
-    if os.path.exists("./tmp.png"):
-        os.remove("./tmp.png")
+    if not os.path.exists("temp"):
+        os.makedirs("temp")
+    if os.path.exists(_temp_path):
+        os.remove(_temp_path)
     adb_run("shell screencap -p /data/screenshot.png")
-    adb_run("pull /data/screenshot.png ./tmp.png")
-    if not os.path.exists("./tmp.png"):
+    adb_run(f"pull /data/screenshot.png {_temp_path}")
+    if not os.path.exists(_temp_path):
         print("Screenshot error.")
         return -1
     return 0
@@ -47,7 +55,7 @@ def adb_click(center, rand_range=7):
     adb_run(f"shell input tap {x} {y}")
 
 
-def image_to_pos(target, template):
+def image_to_pos(target, template, type = cv2.TM_CCOEFF_NORMED):
     targ_img = cv2.imread(target)
     temp_img = cv2.imread(template)
     assert (
@@ -57,22 +65,32 @@ def image_to_pos(target, template):
         temp_img is not None
     ), "template file could not be read, check with os.path.exists()"
     image_h, image_w = temp_img.shape[:2]
-    result = cv2.matchTemplate(targ_img, temp_img, cv2.TM_CCOEFF_NORMED)
+    result = cv2.matchTemplate(targ_img, temp_img, type)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
     # print("prob:", max_val)
-    if max_val > 0.95:
-        center = (max_loc[0] + image_w / 2, max_loc[1] + image_h / 2)
-        return center
+    global __last_pos
+    if type == cv2.TM_CCOEFF_NORMED:
+        if max_val > 0.95:
+            center = (max_loc[0] + image_w / 2, max_loc[1] + image_h / 2)
+            __last_pos = center
+            return center
+        else:
+            return False
     else:
-        return False
+        if min_val < 0.02:
+            center = (min_loc[0] + image_w / 2, min_loc[1] + image_h / 2)
+            __last_pos = center
+            return center
+        else:
+            return False
 
 
-def image_exists(target, template):
-    pos = image_to_pos(target, template)
+def image_exists(target, template, type = cv2.TM_CCOEFF_NORMED):
+    pos = image_to_pos(target, template, type)
     return pos != False
 
 
-def image_press(template, colddown=0.5, target="tmp.png"):
+def image_press(template, colddown=0.5, target=_temp_path):
     adb_screenshot()
     pos = image_to_pos(target, template)
     if pos != False:
@@ -86,66 +104,104 @@ def image_press(template, colddown=0.5, target="tmp.png"):
 def stamina_recover():
     while True:
         adb_screenshot()
-        if image_exists("tmp.png", "data/use.png"):
-            pos = image_to_pos("tmp.png", "data/use.png")
+        if image_exists(_temp_path, "data/use.png"):
+            pos = __last_pos
             adb_click(pos)
-        elif image_exists("tmp.png", "data/ok.png"):
-            pos = image_to_pos("tmp.png", "data/ok.png")
+        elif image_exists(_temp_path, "data/ok.png"):
+            pos = __last_pos
             adb_click(pos)
             return
-        elif image_exists("tmp.png", "data/stamina_limited.png"):
-            pos = image_to_pos("tmp.png", "data/stamina_limited.png")
+        elif image_exists(_temp_path, "data/stamina_limited.png"):
+            pos = __last_pos
             adb_click(pos)
-        elif image_exists("tmp.png", "data/stamina_small.png"):
-            pos = image_to_pos("tmp.png", "data/stamina_small.png")
+        elif image_exists(_temp_path, "data/stamina_small.png"):
+            pos = __last_pos
             adb_click(pos)
-        elif image_exists("tmp.png", "data/stamina_mid.png"):
-            pos = image_to_pos("tmp.png", "data/stamina_mid.png")
+        elif image_exists(_temp_path, "data/stamina_mid.png"):
+            pos = __last_pos
             adb_click(pos)
-        elif image_exists("tmp.png", "data/stamina_large.png"):
-            pos = image_to_pos("tmp.png", "data/stamina_large.png")
+        elif image_exists(_temp_path, "data/stamina_large.png"):
+            pos = __last_pos
             adb_click(pos)
         time.sleep(0.2)
+
+
+def string_get_digits(_string):
+    return ''.join(list(filter(str.isdigit, _string)))
+
+
+def acquire_bonus():
+    print("进入无限池页面。")
+    while True:
+        adb_screenshot()
+        if image_exists(_temp_path, "data/refresh_pool.png"):
+            print("重置无限池。")
+            pos = __last_pos
+            adb_click(pos)
+        elif image_exists(_temp_path, "data/ok.png"):
+            pos = __last_pos
+            adb_click(pos)
+        elif image_exists(_temp_path, "data/gacha.png", cv2.TM_SQDIFF_NORMED):
+            print("抽取无限池。")
+            pos = __last_pos
+            adb_click(pos)
+        elif image_exists(_temp_path, "data/bonus_indicator.png"):
+            img = Image.open(_temp_path)
+            # Get coin remains
+            cropImg = img.crop((123, 520, 205, 570))
+            # cropImg.save("temp/get_digit.png")
+            content = pytesseract.image_to_string(cropImg, lang="eng", config="--psm 6")
+            coin_remain = string_get_digits(content)
+            if coin_remain != "" and int(coin_remain) < 10:
+                image_press("data/back.png")
+        elif image_exists(_temp_path, "data/raid_event_main.png"):
+            print("无限池币数目不足。返回主页面。")
+            return
+        time.sleep(0.25)
 
 
 def main():
     state = 0
     battle_start_time = 0
-    BATTLE_TIME_THRESHOLD = 45
+    BATTLE_TIME_THRESHOLD = 30
     battle_rounds = 0
     battle_rounds_failure = 0
     program_start_time = time.time()
-    combo_tap = False
+    auto_tap = False
+    goto_bonus = True
     print("等待进入主界面。")
-    if combo_tap:
+    if auto_tap:
         print("注意：启用了连击。")
     while state >= 0:
         bad = adb_screenshot()
         if state == 0:  # Waiting for main page.
-            pos = image_to_pos("tmp.png", "data/raid_event_main.png")
+            pos = image_to_pos(_temp_path, "data/raid_event_main.png")
             if pos != False:
                 print("检测到已经进入主界面。")
-                # Switch to HELL Difficulty.
-                pos = image_to_pos("tmp.png", "data/raid_event_hell_diff.png")
+                if goto_bonus:
+                    while not image_press("data/bonus.png"):
+                        continue
+                    acquire_bonus()
+                    goto_bonus = False
+                # Goto prepare page.
+                pos = image_to_pos(_temp_path, "data/raid_event_hell_diff.png")
                 if pos != False:
                     print("切换到准备界面。")
                     adb_click(pos)
                     state = 1
-                else:
-                    print("获取位置失败。")
-            pos = image_to_pos("tmp.png", "data/close.png")
+            pos = image_to_pos(_temp_path, "data/close.png")
             if pos != False:
                 adb_click(pos)
             time.sleep(0.25)
         elif state == 1:  # In Prepare State.
             # Check if auto is enabled.
-            if image_exists("tmp.png", "data/raid_event_hell_prepare.png"):
-                pos = image_to_pos("tmp.png", "data/auto_disabled.png")
+            if image_exists(_temp_path, "data/raid_event_hell_prepare.png"):
+                pos = image_to_pos(_temp_path, "data/auto_disabled.png")
                 if pos != False:
                     print("检测到自动模式关闭。打开自动模式。")
                     adb_click(pos)
                     time.sleep(0.75)
-                pos = image_to_pos("tmp.png", "data/raid_event_go.png")
+                pos = image_to_pos(_temp_path, "data/raid_event_go.png")
                 if pos != False:
                     adb_click(pos)
                     # Switch to battle waiting state.
@@ -153,47 +209,58 @@ def main():
             time.sleep(0.25)
         elif state == 2:  # Battle State
             # If jump back to prepare state (Game over)
-            if image_exists("tmp.png", "data/raid_event_hell_prepare.png"):
+            if image_exists(_temp_path, "data/raid_event_hell_prepare.png"):
                 state = 1
             # Else if time limit exceeds
             elif time.time() - battle_start_time > BATTLE_TIME_THRESHOLD:
                 print("超时，重试战斗。")
                 battle_rounds_failure += 1
                 # Battle retry.
-                while image_press("data/pause.png"):
+                while not image_press("data/pause.png"):
                     continue
-                while image_press("data/battle_retry.png"):
+                while not image_press("data/battle_retry.png"):
                     continue
-                while image_press("data/ok.png"):
+                while not image_press("data/ok.png"):
                     continue
                 state = 10
             # Else if quest clear
-            elif image_exists("tmp.png", "data/quest_clear.png"):
+            elif image_exists(_temp_path, "data/quest_clear.png"):
                 print(
                     f"QUEST CLEAR ! Time used: {time.time() - battle_start_time:.2f}s"
                 )
+                # Every 100 rounds get to bonus page
+                if (
+                    battle_rounds - battle_rounds_failure
+                ) % 100 == 0 and battle_rounds - battle_rounds_failure > 0:
+                    print("准备前往领取奖励。")
+                    goto_bonus = True
                 state = 3
-            elif combo_tap:
+            elif auto_tap:
                 adb_click([300, 300])
             time.sleep(0.25)
         elif state == 3:  # Clear stage
-            if image_exists("tmp.png", "data/continue.png"):
+            if image_exists(_temp_path, "data/continue.png"):
                 # Disable show if enabled.
-                pos = image_to_pos("tmp.png", "data/show_enabled.png")
+                pos = image_to_pos(_temp_path, "data/show_enabled.png")
                 if pos != False:
                     adb_click(pos)
                     time.sleep(0.25)
-                pos = image_to_pos("tmp.png", "data/continue.png")
+                pos = image_to_pos(_temp_path, "data/continue.png")
                 adb_click(pos)
-            elif image_exists("tmp.png", "data/retry_after_success.png"):
-                pos = image_to_pos("tmp.png", "data/retry_after_success.png")
+            elif goto_bonus and image_exists(_temp_path, "data/quest_ok.png"):
+                pos = __last_pos
+                adb_click(pos)
+                state = 0
+                print("结算完毕，前往领取奖励。")
+            elif not goto_bonus and image_exists(_temp_path, "data/retry_after_success.png"):
+                pos = __last_pos
                 adb_click(pos)
                 state = 1
                 print("结算完毕，回到准备界面。")
-            elif image_exists("tmp.png", "data/close.png"):
-                pos = image_to_pos("tmp.png", "data/close.png")
+            elif image_exists(_temp_path, "data/close.png"):
+                pos = __last_pos
                 adb_click(pos)
-            elif image_exists("tmp.png", "data/raid_event_hell_prepare.png"):
+            elif image_exists(_temp_path, "data/raid_event_hell_prepare.png"):
                 print("出现了某种错误。回到准备界面。")
                 battle_rounds_failure += 1
                 state = 1
@@ -201,7 +268,7 @@ def main():
                 adb_click([300, 300])  # Speedup screen
             time.sleep(0.2)
         elif state == 10:  # Battle Waiting State
-            if image_exists("tmp.png", "data/battle_indicator.png"):
+            if image_exists(_temp_path, "data/battle_indicator.png"):
                 state = 2
                 battle_start_time = time.time()
                 battle_rounds += 1
@@ -215,9 +282,9 @@ def main():
                     print(
                         f"平均速度为 {(time.time() - program_start_time)/(battle_rounds - 1 - battle_rounds_failure):.2f} 秒/轮。"
                     )
-                if combo_tap:
+                if auto_tap:
                     adb_click([300, 300])
-            elif image_exists("tmp.png", "data/stamina_low.png"):
+            elif image_exists(_temp_path, "data/stamina_low.png"):
                 stamina_recover()
             time.sleep(0.25)
         else:
