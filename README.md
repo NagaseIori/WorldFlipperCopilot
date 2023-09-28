27164dc7b4ca401037c1185d12b9bc49192e2dc9
Rin <31349569+NagaseIori@users.noreply.github.com>
2023-09-28 23:34:32 +0800

    multi devices support

---
+++ README.md
@@ -0,0 +1,15 @@
+# WorldFlipperCopilot
+
+## Installation
+
+```
+pip install -r requirements.txt
+```
+
+You should also install `tesseract` and add it to your PATH. To check if you've already installed `tesseract`, use `tesseract --version`.
+
+## Usage
+
+```
+python src/main.py
+```
--- src/main.py
+++ src/main.py
@@ -5,38 +5,50 @@ import subprocess
 import random
 import pytesseract
 import re
+import json
 from PIL import Image
 
 __last_pos = False
 _temp_path = "temp/temp.png"
 battle_rounds = 0
 battle_rounds_failure = 0
+__DEBUG = False
+
+def output_log(log):
+    global device_tag
+    print(f"[{device_tag}] {log}")
 
 def adb_run(serial_num, command):
-    return subprocess.run(
-        f"adb -s {serial_num} {command}", stdout=subprocess.DEVNULL, check=True
-    )
-
-
-def adb_run(command):
-    return subprocess.run(f"adb {command}", stdout=subprocess.DEVNULL, check=True)
+    if __DEBUG:
+        return subprocess.run(
+            f"adb -s {serial_num} {command}", check=True
+        )
+    else:
+        return subprocess.run(
+            f"adb -s {serial_num} {command}", stdout=subprocess.DEVNULL, check=True
+        )
 
 
 def adb_screenshot():
+    global device_id
     if not os.path.exists("temp"):
         os.makedirs("temp")
     if os.path.exists(_temp_path):
         os.remove(_temp_path)
-    adb_run("shell screencap -p /data/screenshot.png")
-    adb_run(f"pull /data/screenshot.png {_temp_path}")
+    adb_run(device_id, "shell screencap -p /data/screenshot.png")
+    adb_run(device_id, f"pull /data/screenshot.png {_temp_path}")
     if not os.path.exists(_temp_path):
         print("Screenshot error.")
         return -1
     return 0
 
 
-def adb_connect(address):
+def adb_reset():
     os.system("adb kill-server")
+    os.system("adb start-server")
+
+
+def adb_connect(address):
     result = os.popen(f"adb connect {address}")
     if result.read().find("connected") == -1:
         print("Connect failed.")
@@ -47,13 +59,14 @@ def adb_connect(address):
 
 
 def adb_click(center, rand_range=7):
+    global device_id
     if center == False:
         return
     (x, y) = center
     x += random.randint(-rand_range, rand_range)
     y += random.randint(-rand_range, rand_range)
     # os.system(f"adb shell input tap {x} {y}")
-    adb_run(f"shell input tap {x} {y}")
+    adb_run(device_id, f"shell input tap {x} {y}")
 
 
 def image_to_pos(target, template, type=cv2.TM_CCOEFF_NORMED):
@@ -141,7 +154,7 @@ def string_get_digits(_string):
 
 
 def acquire_bonus():
-    print("进入无限池页面。")
+    output_log("进入无限池页面。")
     while True:
         adb_screenshot()
         if error_check():
@@ -174,34 +187,40 @@ def acquire_bonus():
 
 def error_check():
     if image_exists(_temp_path, "data/date_change.png"):
-        print("检测到日期改变。")
+        output_log("检测到日期改变。")
         return True
     if image_exists(_temp_path, "data/start_page.png"):
-        print("检测到退回主页面。")
+        output_log("检测到退回主页面。")
         return True
     if image_exists(_temp_path, "data/error_0.png"):
-        print("检测到联网错误。")
+        output_log("检测到联网错误。")
         return True
     return False
 
 
-def raid_event():
+def raid_event(tconfig):
     state = 0
     battle_start_time = 0
-    BATTLE_TIME_THRESHOLD = 30
-    BONUS_ROUND = 100
+    BATTLE_TIME_THRESHOLD = tconfig.get("timeout", -1)
+    BONUS_ROUND = tconfig.get("bonus_round", 100)
     global battle_rounds
     global battle_rounds_failure
     program_start_time = time.time()
-    auto_tap = False
+    # auto_tap = False
+    auto_tap = tconfig.get("auto_tap", False)
     goto_bonus = True
-    auto_close_at_beginning = False
+
     auto_close_timer = 0
-    AUTO_CLOSE_TIME = 1.0
+    auto_closed = False
+    limit_rounds = tconfig.get("limit_round", -1)
+    auto_close_conf = tconfig.get("auto_close", {"enabled": False, "time": 0})
+    auto_close_at_beginning = auto_close_conf["enabled"]
+    AUTO_CLOSE_TIME = auto_close_conf["time"]
+
     AUTO_POSITION = (44, 189)
-    print("进入 RAID EVENT 活动主界面。")
+    output_log("进入 RAID EVENT 活动主界面。")
     if auto_tap:
-        print("注意：启用了连击。")
+        output_log("注意：启用了连击。")
     while state >= 0:
         bad = adb_screenshot()
         if error_check():
@@ -209,7 +228,7 @@ def raid_event():
         if state == 0:  # Waiting for main page.
             pos = image_to_pos(_temp_path, "data/raid_event_main.png")
             if pos != False:
-                print("检测到已经进入主界面。")
+                output_log("检测到已经进入主界面。")
                 if goto_bonus:
                     pos = image_to_pos(_temp_path, "data/bonus.png")
                     if pos != False:
@@ -221,7 +240,7 @@ def raid_event():
                 else:
                     pos = image_to_pos(_temp_path, "data/raid_event_hell_diff.png")
                     if pos != False:
-                        print("切换到准备界面。")
+                        output_log("切换到准备界面。")
                         adb_click(pos)
                         state = 1
             pos = image_to_pos(_temp_path, "data/close.png")
@@ -233,11 +252,11 @@ def raid_event():
             if image_exists(_temp_path, "data/raid_event_hell_prepare.png"):
                 pos = image_to_pos(_temp_path, "data/auto_disabled.png")
                 if pos != False:
-                    print("检测到自动模式关闭。打开自动模式。")
+                    output_log("检测到自动模式关闭。打开自动模式。")
                     adb_click(pos)
                     time.sleep(0.2)
                 # if image_exists(_temp_path, "data/auto_continue_disabled.png"):
-                #     print("检测到自动续战关闭。打开自动续战。")
+                #     output_log("检测到自动续战关闭。打开自动续战。")
                 #     adb_click(__last_pos)
                 #     time.sleep(0.2)
                 pos = image_to_pos(_temp_path, "data/raid_event_go.png")
@@ -249,38 +268,41 @@ def raid_event():
         elif state == 2:  # Battle State
             # Auto close for one sec
             if auto_close_at_beginning:
-                if auto_close_timer == 0:
-                    if image_exists(_temp_path, "data/auto_enabled_x1.png"):
-                        adb_click(__last_pos)
-                    elif image_exists(_temp_path, "data/auto_disabled_x0.png"):
-                        auto_close_timer = time.time()
-                elif time.time() - auto_close_timer > AUTO_CLOSE_TIME and image_exists(_temp_path, "data/auto_disabled_x0.png"):
-                    adb_click(__last_pos)
+                if time.time() - auto_close_timer > AUTO_CLOSE_TIME and auto_closed:
+                    adb_click(AUTO_POSITION)
+                    time.sleep(0.05)
+                    adb_click((300, 300))
+                    auto_closed = False
             # If jump back to prepare state (Game over)
             if image_exists(_temp_path, "data/raid_event_hell_prepare.png"):
                 state = 1
             # Else if time limit exceeds
-            elif time.time() - battle_start_time > BATTLE_TIME_THRESHOLD:
-                print("超时，重试战斗。")
+            elif (
+                BATTLE_TIME_THRESHOLD > 0
+                and time.time() - battle_start_time >= BATTLE_TIME_THRESHOLD
+            ):
+                output_log("超时，重试战斗。")
                 battle_rounds_failure += 1
                 state = 4
             # Else if quest clear
-            elif image_exists(_temp_path, "data/quest_clear.png") or image_exists(
-                _temp_path, "data/quest_clear_2.png"
+            elif (
+                image_exists(_temp_path, "data/quest_clear.png")
+                or image_exists(_temp_path, "data/quest_clear_2.png")
+                or image_exists(_temp_path, "data/stage_clear.png")
             ):
-                print(
+                output_log(
                     f"QUEST CLEAR ! Time used: {time.time() - battle_start_time:.2f}s"
                 )
                 # Every 100 rounds get to bonus page
                 if (
                     battle_rounds - battle_rounds_failure
                 ) % BONUS_ROUND == 0 and battle_rounds - battle_rounds_failure > 0:
-                    print(f"检测到已经通过 {BONUS_ROUND} 轮。准备前往领取奖励。")
+                    output_log(f"检测到已经通过 {BONUS_ROUND} 轮。准备前往领取奖励。")
                     goto_bonus = True
                 state = 3
             elif auto_tap:
                 adb_click([300, 300])
-            time.sleep(0.25)
+            time.sleep(0.1)
         elif state == 3:  # Clear stage
             if image_exists(_temp_path, "data/continue.png"):
                 # Disable show if enabled.
@@ -294,33 +316,42 @@ def raid_event():
                 pos = __last_pos
                 adb_click(pos)
                 state = 0
-                print("结算完毕，前往领取奖励。")
+                output_log("结算完毕，前往领取奖励。")
             elif not goto_bonus and image_exists(
                 _temp_path, "data/retry_after_success.png"
             ):
                 pos = __last_pos
                 adb_click(pos)
                 state = 1
-                print("结算完毕，回到准备界面。")
+                output_log("结算完毕，回到准备界面。")
+                if limit_rounds > 0 and battle_rounds >= limit_rounds:
+                    output_log("战斗达到限制上限。退出该任务。")
+                    return
             elif image_exists(_temp_path, "data/close.png"):
                 pos = __last_pos
                 adb_click(pos)
             elif image_exists(_temp_path, "data/raid_event_hell_prepare.png"):
-                print("出现了某种错误。回到准备界面。")
+                output_log("出现了某种错误。回到准备界面。")
                 battle_rounds_failure += 1
                 state = 1
             else:
                 general_check()  # Speedup screen
             time.sleep(0.2)
         elif state == 4:  # Battle retrying State
-            if image_exists(_temp_path, "data/pause.png") or image_exists(
+            if image_exists(_temp_path, "data/stage_clear.png"):
+                state = 3
+            elif image_exists(_temp_path, "data/raid_event_hell_prepare.png"):
+                state = 1
+            elif image_exists(_temp_path, "data/raid_event_main.png"):
+                state = 0
+            elif image_exists(_temp_path, "data/pause.png") or image_exists(
                 _temp_path, "data/battle_retry.png"
             ):
                 adb_click(__last_pos)
             elif image_exists(_temp_path, "data/ok.png"):
                 adb_click(__last_pos)
                 state = 10
-            time.sleep(0.1)
+            time.sleep(0.2)
         elif state == 10:  # Battle Waiting State
             if image_exists(_temp_path, "data/battle_indicator.png"):
                 state = 2
@@ -330,23 +361,33 @@ def raid_event():
                 failure_rate = 0.0
                 if battle_rounds > 1:
                     failure_rate = battle_rounds_failure / (battle_rounds - 1) * 100
-                print(f"战斗开始。当前第 {battle_rounds} 轮，翻车率为 {failure_rate:.2f}%。")
-                print(f"目前通过共 {battle_rounds - 1 - battle_rounds_failure} 轮。")
-                print(f"总耗时为 {time.time() - program_start_time:.2f}s。")
+                output_log(f"战斗开始。当前第 {battle_rounds} 轮，翻车率为 {failure_rate:.2f}%。")
+                output_log(f"目前通过共 {battle_rounds - 1 - battle_rounds_failure} 轮。")
+                output_log(f"总耗时为 {time.time() - program_start_time:.2f}s。")
                 if battle_rounds - 1 - battle_rounds_failure > 0:
-                    print(
+                    output_log(
                         f"平均速度为 {(time.time() - program_start_time)/(battle_rounds - 1 - battle_rounds_failure):.2f} 秒/轮。"
                     )
                 if auto_tap:
                     adb_click([300, 300])
+                if auto_close_at_beginning:
+                    adb_click(AUTO_POSITION)
+                    auto_close_timer = time.time()
+                    auto_closed = True
             elif image_exists(_temp_path, "data/stamina_low.png"):
                 stamina_recover()
-            time.sleep(0.25)
+            time.sleep(0.1)
         else:
             time.sleep(0.75)
 
 
-def main():
+def main(dev_config):
+    global device_id
+    device_id = dev_config["address"]
+    global device_tag
+    device_tag = dev_config["tag"]
+    global _temp_path
+    _temp_path = f"temp/temp_{device_id}.png"
     target = "RAID"
     target_state = 1
 
@@ -355,54 +396,88 @@ def main():
         target_state = 1
 
     state = 0
-    print("进入主程序。")
+    output_log("进入主程序。")
+    task_loop = dev_config.get("task_loop", False)
     while True:
-        adb_screenshot()
-        if state == 0:  # If on main page
-            # Page check & State switch
-            if image_exists(_temp_path, "data/raid_event_main.png"):
-                raid_event()
-            elif image_exists(_temp_path, "data/events_main.png"):
-                print("进入活动页面。")
-                state = 1
-            # If on main page
-            elif (
-                image_exists(_temp_path, "data/close.png")
-                or image_exists(_temp_path, "data/close_red.png")
-                or image_exists(_temp_path, "data/ok.png")
-                or image_exists(_temp_path, "data/give_up.png")
-                or image_exists(_temp_path, "data/back.png")
-            ):
-                adb_click(__last_pos)
-            else:
-                if target_state == 1 and image_exists(_temp_path, "data/events.png"):
-                    adb_click(__last_pos)
-                else:
-                    adb_click([1024 / 2, 768 / 2])
-        elif state == 1:  # If on event page
-            if target == "RAID" and image_exists(
-                _temp_path, "data/raid_event_banner.png"
-            ):
-                adb_click(__last_pos)
-                raid_event()
-            elif not image_exists(_temp_path, "data/events_main.png"):
-                print("离开了活动页面。回到主程序。")
-                state = 0
-            else:
-                print("警告 - 未检测到目标 / 未设定目标。")
-        time.sleep(0.5)
+        for task in dev_config["tasks"]:
+            target = task["type"]
+            while True:
+                adb_screenshot()
+                if state == 0:  # If on main page
+                    # Page check & State switch
+                    if image_exists(_temp_path, "data/raid_event_main.png"):
+                        raid_event(task["settings"])
+                    elif image_exists(_temp_path, "data/events_main.png"):
+                        output_log("进入活动页面。")
+                        state = 1
+                    # If on main page
+                    elif (
+                        image_exists(_temp_path, "data/close.png")
+                        or image_exists(_temp_path, "data/close_red.png")
+                        or image_exists(_temp_path, "data/ok.png")
+                        or image_exists(_temp_path, "data/give_up.png")
+                        or image_exists(_temp_path, "data/back.png")
+                    ):
+                        adb_click(__last_pos)
+                    else:
+                        if target_state == 1 and image_exists(
+                            _temp_path, "data/events.png"
+                        ):
+                            adb_click(__last_pos)
+                        else:
+                            adb_click([1024 / 2, 768 / 2])
+                elif state == 1:  # If on event page
+                    if target == "RAID" and image_exists(
+                        _temp_path, "data/raid_event_banner.png"
+                    ):
+                        adb_click(__last_pos)
+                        raid_event(task)
+                        break
+                    elif not image_exists(_temp_path, "data/events_main.png"):
+                        output_log("离开了活动页面。回到主程序。")
+                        state = 0
+                    else:
+                        output_log("警告 - 未检测到目标 / 未设定目标。")
+                time.sleep(0.5)
+        if not task_loop:
+            break
 
+
+def dev_loop(dev):
+    while True:
+        main(dev)
+
+
+import multiprocessing as mp
 
 if __name__ == "__main__":
     print("Script intializing.")
-    # adb_addr = input("ADB Address:")
-    adb_addr = 16384  # ADB Address/Port
-    # adb_addr = 21543
-    # adb_addr = 16448
-    bad = adb_connect(adb_addr)
-    if bad:
-        exit()
+    global config
+    with open("config.json", encoding="utf-8") as f:
+        if not f.readable:
+            print("Config file not exists.")
+            exit()
+        config = json.loads(f.read())
 
+    adb_reset()
     print("Script Initialized. Start main program.")
+
+    devices = config["devices"]
     while True:
-        main()
+        p_list = []
+        for dev in devices:
+            if dev.get("enabled", True):
+                dev.setdefault("tag", "unknown device")
+                try:
+                    bad = adb_connect(dev["address"])
+                    if bad:
+                        raise
+                except:
+                    print(f"Connect failed. Skip device {dev['tag']}")
+                    continue
+                print(f"Start subprocess of device {dev['tag']}")
+                p = mp.Process(target=dev_loop, args=(dev,))
+                p.start()
+                p_list.append(p)
+        for p in p_list:
+            p.join()
